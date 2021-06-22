dd22bc5c1fef755999723aadd1da4a24f24d9e4c
jnphx <buggytot@gmail.com>
2021-06-21 20:42:36 -0700

    When adding new meals, button only shows on ones not added

---
+++ Models/WeeklyMealsViewModels/ChooseMeal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeklyMeals.Models.WeeklyMealsViewModels
+{
+    public class ChooseMeal
+    {
+        public int RecipeID;
+
+        public int MealPlanID;
+
+        public string ImageUrl;
+
+        public int MealPlanRecipeID;
+
+        public string RecipeName;
+
+        public double NumberBatches;
+    }
+}
--- Pages/Recipes/ChooseRecipe.cshtml.cs
+++ Pages/Recipes/ChooseRecipe.cshtml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using WeeklyMeals.Models;
+using WeeklyMeals.Models.WeeklyMealsViewModels;
 
 namespace WeeklyMeals.Pages.Recipes
 {
@@ -16,11 +18,81 @@ namespace WeeklyMeals.Pages.Recipes
             _context = context;
         }
 
-        public IList<Recipe> Recipes { get;set; }
+        public IList<ChooseMeal> Recipes { get; set; }
 
         public async Task OnGetAsync()
         {
-            Recipes = await _context.Recipes.ToListAsync();
+            //Get all recipes joined with MealPlanRecipes, outer join
+            //If match, then show +/- batch buttons, if not match, show add button
+            var mprRecipes = from m in _context.MealPlanRecipes
+                             join u in _context.UserSettings on m.MealPlanID equals u.MealPlanID
+                             select m;
+
+            IQueryable<ChooseMeal> choosemealrecipes = from r in _context.Recipes
+                                                       join mpr in mprRecipes on r.RecipeID equals mpr.RecipeID
+                                                       into MPRGroup
+                                                       from mpr in MPRGroup.DefaultIfEmpty()
+                                                       select new ChooseMeal
+                                                       {
+                                                           RecipeID = r.RecipeID,
+                                                           RecipeName = r.Name,
+                                                           ImageUrl = r.ImageUrl,
+                                                           MealPlanRecipeID = mpr == null ? 0 : mpr.MealPlanRecipeID,
+                                                           NumberBatches = mpr == null ? 0 : mpr.NumberBatches
+                                                       };
+
+            Recipes = await choosemealrecipes.ToListAsync();
+
+            //Recipes = await _context.Recipes.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostCheckDelete(int? MealPlanRecipeID)
+        {
+            MealPlanRecipe mpr = _context.MealPlanRecipes.Find(MealPlanRecipeID);
+
+            if (mpr != null)
+            {
+                if (mpr.NumberBatches == 1)
+                {
+                    //They want to delete this
+                    _context.MealPlanRecipes.Remove(mpr);
+                    _context.SaveChanges();
+                }
+                else
+                {
+                    //Decrement batch count
+                    --mpr.NumberBatches;
+                    if (await TryUpdateModelAsync<MealPlanRecipe>(
+                        mpr,
+                        "",
+                        s => s.NumberBatches))
+                    {
+                        await _context.SaveChangesAsync();
+                    }
+                }
+            }
+
+            return RedirectToPage("/Recipes/ChooseRecipe");
+        }
+
+        public async Task<IActionResult> OnPostCheckAdd(int? MealPlanRecipeID)
+        {
+            MealPlanRecipe mpr = _context.MealPlanRecipes.Find(MealPlanRecipeID);
+
+            if (mpr != null)
+            {
+                //Increment batch count
+                ++mpr.NumberBatches;
+                if (await TryUpdateModelAsync<MealPlanRecipe>(
+                    mpr,
+                    "",
+                    s => s.NumberBatches))
+                {
+                    await _context.SaveChangesAsync();
+                }
+            }
+
+            return RedirectToPage("/Recipes/ChooseRecipe");
         }
 
         public async Task<IActionResult> OnPostAsync(int RecipeID)
